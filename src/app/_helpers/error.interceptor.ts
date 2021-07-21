41007f03e7e9ddf6a67dfaeb4be825c35261e835
batou@mtcaptcha <batou@mtcaptcha.com>
2021-07-21 15:15:22 +0530

    Upgrade dependencies

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -2,23 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AlertService, AuthenticationService } from '@/_services';
 
-import { AuthenticationService } from '@/_services';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+    constructor(private authenticationService: AuthenticationService, private alertService: AlertService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                location.reload(true);
-            }
-            
-            const error = err.error.message || err.statusText;
-            return throwError(error);
+        return next.handle(request).pipe(catchError(err => {            
+            const error = err.error.message;
+            return throwError(err);
         }))
     }
 }
